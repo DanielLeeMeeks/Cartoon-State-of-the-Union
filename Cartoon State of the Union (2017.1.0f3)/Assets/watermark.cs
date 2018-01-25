675545348c93491c665e2855393478614f3b0c79
Daniel Meeks <daniel-meeks@earthlink.net>
2018-01-25 16:31:27 -0500

    Added watermark

---
+++ Cartoon State of the Union (2017.1.0f3)/Assets/watermark.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class watermark : MonoBehaviour {
+
+	int step = 0;
+	public string [] watermarks;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(go());
+	}
+	
+	IEnumerator go (){
+
+		this.GetComponent<Text>().text = watermarks[step];
+
+		yield return new WaitForSeconds(15);
+
+		step++;
+
+		if (step >= watermarks.Length){
+			step = 0;
+		}
+
+		StartCoroutine(go());
+
+	}
+}
