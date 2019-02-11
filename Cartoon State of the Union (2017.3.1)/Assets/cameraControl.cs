743c9240703c30d65632ede4cc762cb4139dca92
Daniel <daniellmeeks@gmail.com>
2019-02-11 17:27:54 -0500

    2019 Update (2017.3.1)

---
+++ Cartoon State of the Union (2017.3.1)/Assets/cameraControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControl : MonoBehaviour {
+
+	public Vector4 [] cameraPositons;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Update(){
+		CameraByButtons(); 
+		CameraByAxis();
+
+	}
+
+	public void setCameraPostition(int id){
+		this.transform.position = new Vector3 (cameraPositons[id].x, cameraPositons[id].y, cameraPositons[id].z);
+		this.GetComponent<Camera>().orthographicSize = cameraPositons[id].w;
+	}
+
+	public void CameraByAxis(){
+		//Debug.Log(Input.GetAxisRaw("dPadX"));
+		if (Input.GetAxisRaw("dPadX") > 0.5f){
+			setCameraPostition(3);
+		}else if (Input.GetAxisRaw("dPadX") < -0.5f){
+			setCameraPostition(1);
+		}else if (Input.GetAxisRaw("dPadY") > 0.5f){
+			setCameraPostition(0);
+		}else if (Input.GetAxisRaw("dPadY")< -0.5f){
+			setCameraPostition(2);
+		}
+	}
+
+	public void CameraByButtons(){
+		if (Input.GetButtonDown("Camera CloseUp")){
+			setCameraPostition(0);
+		} else if (Input.GetButtonDown("Camera Wide")){
+			setCameraPostition(1);
+		} else if (Input.GetButtonDown("Camera Long")){
+			setCameraPostition(2);
+		} else if (Input.GetButtonDown("Camera 4")){
+			setCameraPostition(3);
+		}
+	}
+
+}
---
+++ Cartoon State of the Union (2017.3.1)/Assets/crowd.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class crowd : MonoBehaviour {
+
+	Animator _a;
+	public Text debug;
+	public bool crowdStanding;
+	public headTurn [] people;
+
+	// Use this for initialization
+	void Start () {
+		_a = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Crowd Sit"))
+        {
+            crowdStanding = false;
+            _a.SetTrigger("sit"); debug.text = "Crowd: Sitting";
+            foreach (headTurn p in people)
+            {
+                p.sit();
+            }
+        }
+        else if (Input.GetButtonDown("Crowd Clap"))
+        {
+            crowdStanding = true;
+            _a.SetTrigger("clap"); debug.text = "Crowd: Clapping";
+            foreach (headTurn p in people)
+            {
+                p.stand();
+                p.clap();
+            }
+        }
+        else if (Input.GetButtonDown("Crowd Clap Left")) {
+            crowdStanding = true;
+            _a.SetTrigger("clap_left"); debug.text = "Crowd: Clapping (Left)";
+            people[0].sit();
+            people[1].stand();
+            people[1].clap();
+
+        }
+        else if (Input.GetButtonDown("Crowd Clap Right"))
+        {
+             crowdStanding = true;
+            _a.SetTrigger("clap_right"); debug.text = "Crowd: Clapping (Right)";
+            people[1].sit();
+            people[0].stand();
+            people[0].clap();
+
+        }
+    }
+}
